cf4952d274182d12a2c56eb3e69369326a6fb9f4
Eric Snyder TPC <SnyderE@ipslazure.onmicrosoft.com>
2025-01-28 09:29:37 +0000

    Trained the model up to 3500 iters, which showed a good deal of overfitting (the model is sometimes / often duplicating the input.txt).

--- train.py
+++ train.py
@@ -60,7 +60,7 @@ dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
 bias = False # do we use bias inside LayerNorm and Linear layers?
 # adamw optimizer
 learning_rate = 6e-4 # max learning rate
-max_iters = 400 # 2000 for shakespeare - # 600000 # for OpenWebText - (not on my slow sys) total number of training iterations
+max_iters = 3500 # 2000 for shakespeare - # 600000 # for OpenWebText - (not on my slow sys) total number of training iterations
 weight_decay = 1e-1
 beta1 = 0.9
 beta2 = 0.95
